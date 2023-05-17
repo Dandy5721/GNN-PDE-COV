f28ca314feb845821be3313d9c75c6c0ec465bea
Tingting Dan <58943382+Dandy5721@users.noreply.github.com>
2023-05-17 09:49:10 -0400

    Add files via upload

---
+++ scripts+/GRAND+/src/cora.sh
@@ -0,0 +1,7 @@
+python run_GNN.py --dataset 'Cora' --planetoid_split --time 2.0 --gpu 1 > exp/"Cora_2.0_GTV_1".txt
+python run_GNN.py --dataset 'Cora' --planetoid_split --time 4.0 --gpu 1 > exp/"Cora_4.0_GTV_1".txt
+python run_GNN.py --dataset 'Cora' --planetoid_split --time 8.0 --gpu 1 > exp/"Cora_8.0_GTV_1".txt
+python run_GNN.py --dataset 'Cora' --planetoid_split --time 16.0 --gpu 1 > exp/"Cora_16.0_GTV_1".txt
+python run_GNN.py --dataset 'Cora' --planetoid_split --time 32.0 --gpu 1 > exp/"Cora_32.0_GTV_1".txt
+python run_GNN.py --dataset 'Cora' --planetoid_split --time 64.0 --gpu 1 > exp/"Cora_64.0_GTV_1".txt
+python run_GNN.py --dataset 'Cora' --planetoid_split --time 128.0 --gpu 1 > exp/"Cora_128.0_GTV_1".txt
---
+++ scripts+/GRAND+/src/pubmed.sh
@@ -0,0 +1,7 @@
+python run_GNN.py --dataset 'Pubmed' --planetoid_split --time 2.0 --epoch 200 --gpu 1 > exp/"Pubmed_2.0_GTV_5".txt
+python run_GNN.py --dataset 'Pubmed' --planetoid_split --time 4.0 --epoch 200 --gpu 1 > exp/"Pubmed_4.0_GTV_5".txt
+python run_GNN.py --dataset 'Pubmed' --planetoid_split --time 8.0 --epoch 200 --gpu 1 > exp/"Pubmed_8.0_GTV_5".txt
+python run_GNN.py --dataset 'Pubmed' --planetoid_split --time 16.0 --epoch 200 --gpu 1 > exp/"Pubmed_16.0_GTV_5".txt
+python run_GNN.py --dataset 'Pubmed' --planetoid_split --time 32.0 --epoch 200 --gpu 1 > exp/"Pubmed_32.0_GTV_5".txt
+python run_GNN.py --dataset 'Pubmed' --planetoid_split --time 64.0 --epoch 200 --gpu 1 > exp/"Pubmed_64.0_GTV_5".txt
+python run_GNN.py --dataset 'Pubmed' --planetoid_split --time 128.0 --epoch 200 --gpu 1 > exp/"Pubmed_128.0_GTV_5".txt
